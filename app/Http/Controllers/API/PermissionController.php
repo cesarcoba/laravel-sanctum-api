e9f54d39d042d1554d8409adbd27c5edef444134
César Cobá Couoh <cobacesar@hotmail.com>
2025-09-29 22:34:22 -0600

    add roles and permissions

---
+++ app/Http/Controllers/API/PermissionController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Permission::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|unique:permissions,name',
+        ]);
+
+        $permission = Permission::create(['name' => $validated['name']]);
+        return response()->json($permission, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\PermissionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,5 +18,13 @@ Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
         Route::post('logout', 'AuthenticationController@logOut')->name('logout');
+
+        // Roles
+        Route::apiResource('roles', RoleController::class)->except(['create', 'edit']);
+
+        // Permissions
+        Route::get('/permissions', [PermissionController::class, 'index']);
+        Route::post('/permissions', [PermissionController::class, 'store']);
+
     });
 });
